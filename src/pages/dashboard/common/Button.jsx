854ac9b934c6fcf5791bd751be6e95ee9cb74e29
md-ali-0 <mohammad.98482@gmail.com>
2023-12-30 01:29:36 +0600

    Buttons Added

--- src/pages/dashboard/common/Button.jsx
+++ src/pages/dashboard/common/Button.jsx
@@ -1,7 +1,20 @@
 const Button = () => {
     return (
         <div>
-            
+            <div className="mb-10">
+                <h2 className="mb-2 text-3xl font-bold">Button</h2>
+                <p className="text-md">
+                    Button is used to start an instant operation and is used
+                    when triggers an action or event
+                </p>
+            </div>
+
+            <div className="bg-white rounded border py-5 px-8 space-x-5">
+                <button className="btn btn-primary">Primary</button>
+                <button className="btn btn-secondary">Secondary</button>
+                <button className="btn btn-info">Info</button>
+                <button className="btn btn-danger">Danger</button>
+            </div>
         </div>
     );
 };
